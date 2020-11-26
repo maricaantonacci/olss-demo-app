515fe9a22dde124c5df030212860956bdc6c34dc
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2020-11-26 14:22:30 +0100

    First import

---
+++ src/main/java/it/infn/olssdemoapp/OlssDemoAppApplication.java
@@ -0,0 +1,13 @@
+package it.infn.olssdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlssDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlssDemoAppApplication.class, args);
+	}
+
+}
