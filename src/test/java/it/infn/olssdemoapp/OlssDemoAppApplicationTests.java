3012918d4f5d369d6526dd23c7ef52ac4981939e
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2020-11-26 19:33:01 +0100

    ...

--- src/test/java/it/infn/olssdemoapp/OlssDemoAppApplicationTests.java
+++ src/test/java/it/infn/olssdemoapp/OlssDemoAppApplicationTests.java
@@ -1,13 +1,10 @@
 package it.infn.olssdemoapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OlssDemoAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
