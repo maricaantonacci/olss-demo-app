38606224de4121e6e4e45a980944c49bc69ac5d4
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2021-04-24 19:08:45 +0200

    Added tests & native image build

--- src/test/java/it/infn/olssdemoapp/OlssDemoAppApplicationTests.java
+++ src/test/java/it/infn/olssdemoapp/OlssDemoAppApplicationTests.java
@@ -1,10 +1,45 @@
 package it.infn.olssdemoapp;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class OlssDemoAppApplicationTests {
 
 
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  public void helloApiReturnsGreeting() throws Exception {
+    mvc.perform(get("/api/hello"))
+      .andExpect(status().isOk())
+      .andExpect(content().string(startsWith("Hello world (from")));
+  }
+
+  @Test
+  public void memoryHogApiReturnsValue() throws Exception {
+    mvc.perform(post("/api/memory/grow"))
+      .andExpect(status().isOk())
+      .andExpect(content().string(is("10000000")));
+  }
+
+  @Test
+  public void memoryHogClearApiReturnsValue() throws Exception {
+    mvc.perform(post("/api/memory/clear"))
+      .andExpect(status().isOk())
+      .andExpect(content().string(is("0")));
+  }
 
 }
